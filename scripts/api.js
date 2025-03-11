4dc3085f1c397bf9b01d3caeb8c1b578859faee9
Quentin <118888713+QuentinB0@users.noreply.github.com>
2025-03-11 16:45:15 +0100

    change throw by a console.error to prevent stops

--- scripts/api.js
+++ scripts/api.js
@@ -2,7 +2,8 @@ export async function fetchFromAPI(endpoint) {
 	try {
 		let res = await fetch("https://jsonplaceholder.typicode.com" + endpoint);
 		if (!res.ok) {
-			throw new Error("Network response was not ok");
+			console.error("Erreur de réseau:", res.status);
+			return;
 		}
 		let data = await res.json();
 		return data.title; // Assurez-vous que l'API retourne bien un objet avec une propriété 'title'
